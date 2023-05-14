a1868afaacc60b8c11213666716e09cf8eb8cd12
Victor Plotnikov <victor.plotnikov7@gmail.com>
2023-05-14 16:54:46 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 12250;
+        int mile = 20;
+        int bonus = (ticketPrice / mile);
+
+        System.out.println("Количество бонусных миль:" + bonus);
+
+    }
+}
